b4142094ce8c3281c696ea5999c555f18187d30e
a5507203 <yaoyu5507203@gmail.com>
2018-08-31 13:52:11 +1000

    host change

--- js/Config.js
+++ js/Config.js
@@ -11,6 +11,6 @@ var Config = {
     lineColor : new THREE.Color(0x0000ff),
     maxLengthAdd: 100000,
     totalLeft: 100000,
-    host:'http://http://13.237.82.173'
+    host:'http://13.237.82.173'
     // host:'http://localhost:3000'
 };
